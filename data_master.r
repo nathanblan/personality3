2f1bd91c4693906271641ab821e7456207faa60f
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-07-21 08:06:22 -0700

    Renamed to second version of data visualizations to data_master

--- data_master.r
+++ data_master.r
@@ -3,6 +3,7 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(stringr)
+library(modelr)
 
 # load data
 p_data <- 
@@ -11,3 +12,15 @@ p_data <-
   mutate(id = row_number()) %>% 
   select(id, everything())
 View(p_data)
+names(p_data)
+
+# split data into 4 data sets !! Add column "id" to all dataframes !!
+
+# raw responses
+
+# raw response time
+
+# everything else
+
+# summary stats
+
