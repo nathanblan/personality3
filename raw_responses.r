46a6203cd4fa973b3be1a2bf5b68a6291b1ac359
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-07-26 22:11:36 -0700

    update

--- raw_responses.r
+++ raw_responses.r
@@ -21,14 +21,29 @@ View(r_data)
 names(r_data)
 
 # response distribution function -----------------------------------------------
-r_data %>% 
-  sample_frac(0.01) %>% 
-  select(-id) %>% 
-  gather(question, response) %>% 
-  count(question, response) %>% 
-  mutate(n = n / 1000) %>%  # lets work in thousands
-  ggplot(aes(x = response, y = n)) +
-  geom_col() +
-  facet_wrap(~ question, scales = "free")
+# harder way (that saves a PDF of all plots to desktop)
+GG_save_pdf = function(list, filename){
+  pdf(filename)
+  for (p in list) {
+    print(p)
+  }
+  
+  dev.off()
+  invisible(NULL)
+}
+
+f <- function(one_column){
+  tibble(tmp = one_column) %>% 
+    ggplot(aes(x = tmp)) +
+    geom_bar() +
+    ggtitle(one_column)
+}
+
+response_plots <- list()
+for (i in 2:ncol(r_data)) {
+  response_plots[[i - 1]] <- f(r_data[[i]])
+}
+
+GG_save_pdf(response_plots, "response_plots.pdf")
 
 # linear models ----------------------------------------------------------------
