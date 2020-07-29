097ed3ac51e06594cf9d293c0413d0d7e66bca7d
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-07-28 23:09:58 -0700

    update

--- raw_responses/raw_responses.r
+++ raw_responses/raw_responses.r
@@ -18,14 +18,43 @@ View(r_data)
 names(r_data)
 
 # - keyed ----------------------------------------------------------------------
-inv_keyed <- list(EXT2, EXT4, EXT6,
-                  EXT8, EXT10, EST1,
-                  EST3, EST5, EST6, 
-                  EST7, EST8, EST9, 
-                  EST10, AGR1, AGR3,
-                  AGR5, AGR7, CSN2, 
-                  CSN4, CSN6, CSN8, 
-                  OPN2, OPN4, OPN6)
+inv_data <- r_data %>% 
+  select(EXT2, EXT4, EXT6,
+         EXT8, EXT10, EST1,
+         EST3, EST5, EST6,
+         EST7, EST8, EST9,
+         EST10, AGR1, AGR3,
+         AGR5, AGR7, CSN2,
+         CSN4, CSN6, CSN8,
+         OPN2, OPN4, OPN6)
+
+inv_data <- inv_data %>% 
+  mutate("EXT2" = recode(EXT2, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>% 
+  mutate("EXT4" = recode(EXT4, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>% 
+  mutate("EXT6" = recode(EXT6, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EXT8" = recode(EXT8, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EXT10" = recode(EXT10, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST1" = recode(EST1, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST3" = recode(EST3, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST5" = recode(EST5, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST6" = recode(EST6, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST7" = recode(EST7, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST8" = recode(EST8, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST9" = recode(EST9, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("EST10" = recode(EST10, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("AGR1" = recode(AGR1, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("AGR5" = recode(AGR5, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("AGR5" = recode(AGR5, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("AGR7" = recode(AGR7, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>% 
+  mutate("CSN2" = recode(CSN2, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("CSN4" = recode(CSN4, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("CSN6" = recode(CSN6, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("CSN8" = recode(CSN8, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("OPN2" = recode(OPN2, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("OPN4" = recode(OPN4, `1` = 5, `2` = 4, `4` = 2, `5` = 1)) %>%
+  mutate("OPN6" = recode(OPN6, `1` = 5, `2` = 4, `4` = 2, `5` = 1))
+
+inv_data
 
 # add totals for each column ---------------------------------------------------
 r_data$EXT_sum <- rowSums(r_data[,2:11])
@@ -60,6 +89,5 @@ for (i in 2:ncol(r_data)) {
 
 GG_save_pdf(response_plots, "response_plots.pdf")
 
-# linear models ----------------------------------------------------------------
-lm.fit=lm(EXT_sum∼EXT1:EXT10, data = r_data)
-summary(lm.fit)
+# classification models --------------------------------------------------------
+
