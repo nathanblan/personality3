a4a96fab88e9d839d5e0f27d9e19205112d9b386
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-02 10:31:20 -0700

    new clutering stuff

--- 02_raw_responses/response_clusters.r
+++ 02_raw_responses/response_clusters.r
@@ -13,7 +13,7 @@ r_data_small <- scale(r_data_small)
 names(r_data_small)
 
 #Principle Component Analysis -------------------------------------------------- 
-S <- cov(r_data_small[,2:50]) #S matrix covariance
+S <- cov(r_data_small[,50:56]) #S matrix covariance
 S.variance <- sum(diag(S)) #total variance or sum of eigenvalues of S
 S.eigen <-  eigen(S) #compute eigenvalues and vectors of S
 S.variance # view results of eigen() and variance
@@ -23,19 +23,30 @@ for (s in S.eigen$values) { #print eigenvalues
 plot(S.eigen$values, xlab = 'Eigenvalue Number', ylab = 'Eigenvalue Size', main = 'Scree Graph') +
   lines(S.eigen$values) 
 
-R <- cor(r_data_small[,2:50]) #R matrix correlation
+R <- cor(r_data_small[,50:56]) #R matrix correlation
 R.eigen <-eigen(R)
 for (r in R.eigen$values) { #print eigenvalues
   print(r / sum(R.eigen$values))
 }
+plot(R.eigen$values, xlab = 'Eigenvalue Number', ylab = 'Eigenvalue Size', main = 'Scree Graph') +
+  lines(R.eigen$values) 
 
 #calculate pca 
-r_data_small.pca <- prcomp(r_data_small[,2:50])
+r_data_small.pca <- prcomp(r_data_small[,50:56])
 summary(r_data_small.pca)
-
+names(r_data_small.pca)
+#Principal Variance Explained
+pr.var <- r_data_small.pca$sdev
+pve <- pr.var/sum(pr.var)
+pve
 #pca plot
 pca.plot <- autoplot(r_data_small.pca, data = r_data_small, colour = 'id')
 pca.plot
+plot(pve, xlab=" Principal Component", ylab="Proportion of Variance Explained", 
+     ylim=c(0,1),type='b')
+plot(cumsum(pve), xlab="Principal Component", ylab="Cumulative Proportion of Variance Explained",
+     ylim=c(0,1),
+       type='b')
 
 #initial model -----------------------------------------------------------------
 # Determine number of clusters
@@ -54,7 +65,8 @@ r_data_small <- data.frame(r_data_small, fit$cluster)
 
 # K-Means Clustering with 5 clusters
 fit <- kmeans(r_data_small, 5)
-
+fit$tot.withinss
+fit$withinss #The variation is *quite* large, maybe we didn't scale and center it right? 
 # Cluster Plot against 1st 2 principal components
 
 # vary parameters for most readable graph
