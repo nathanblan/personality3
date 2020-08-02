880c892d8f278fdf3b972f415567ebf48984c379
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-01 22:12:04 -0700

    PCA

--- 02_raw_responses/response_clusters.r
+++ 02_raw_responses/response_clusters.r
@@ -1,10 +1,43 @@
 # classification models --------------------------------------------------------
+#load libraries ----------------------------------------------------------------
+library(ggfortify)
+
+#set up clustering data --------------------------------------------------------
 set.seed(1)
 r_data_small <- r_data %>% 
   sample_frac(0.2) %>% 
-  
-  
-  k_model <- r_data_small %>% 
+  na.omit(r_data)
+
+scale(r_data_small)
+names(r_data_small)
+
+#Principle Component Analysis -------------------------------------------------- 
+S <- cov(r_data_small[,2:50]) #S matrix covariance
+S.variance <- sum(diag(S)) #total variance or sum of eigenvalues of S
+S.eigen <-  eigen(S) #compute eigenvalues and vectors of S
+S.variance # view results of eigen() and variance
+for (s in S.eigen$values) { #print eigenvalues
+  print(s / sum(S.eigen$values))
+}
+plot(S.eigen$values, xlab = 'Eigenvalue Number', ylab = 'Eigenvalue Size', main = 'Scree Graph') +
+  lines(S.eigen$values) 
+
+R <- cor(r_data_small[,2:50]) #R matrix correlation
+R.eigen <-eigen(R)
+for (r in R.eigen$values) { #print eigenvalues
+  print(r / sum(R.eigen$values))
+}
+
+#calculate pca 
+r_data_small.pca <- prcomp(r_data_small[,2:50])
+summary(r_data_small.pca)
+
+#pca plot
+pca.plot <- autoplot(r_data_small.pca, data = r_data_small, colour = 'id')
+pca.plot
+
+#initial model -----------------------------------------------------------------  
+k_model <- r_data_small %>% 
   select(-id) %>% 
   kmeans(50, nstart = 50)
 
