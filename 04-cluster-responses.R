391807852373d291b58867a32649baed8789e836
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-04 20:45:31 -0700

    update

--- 04-cluster-responses.R
+++ 04-cluster-responses.R
@@ -1,10 +1,20 @@
 # response clusters ------------------------------------------------------------
-
 library(tidyverse)
+
 raw <- 
   read_rds("data-clean/raw.rds") %>% 
   na.omit()
 
+ext <- raw %>% 
+  select(EXT01:EXT10)
+est <- raw %>% 
+  select(EST01:EST10)
+agr <- raw %>% 
+  select(AGR01:AGR10)
+csn <- raw %>% 
+  select(CSN01:CSN10)
+ext <- raw %>% 
+  select(OPN01:OPN10)
 # run code below with ALL data not just small
 small <- raw %>% sample_frac(0.05) %>% select(-id)
 
@@ -21,8 +31,15 @@ tibble(
 
 # K means TO DO
 names(small)
+# Determine number of clusters
+wss <- (nrow(ext))*sum(apply(ext,2,var))
+for (i in 2:10) wss[i] <- sum(kmeans(ext,
+                                     centers=i)$withinss)
+plot(1:10, wss, type="b", xlab="Number of Clusters",
+     ylab="Within groups sum of squares")
+
 fit.ext <- kmeans(small, 5)
-fit.ext$centers %>% #plot means of each cluster, representing how high people each cluster scored
+p1 <- fit.ext$centers %>% #plot means of each cluster, representing how high people each cluster scored
   as_tibble() %>% 
   select(EXT01:EXT10) %>% 
   mutate(id = row_number()) %>% 
@@ -32,7 +49,7 @@ fit.ext$centers %>% #plot means of each cluster, representing how high people ea
   coord_flip()
 
 fit.est <- kmeans(small, 5)
-fit.est$centers %>% #plot means of each cluster, representing how high people each cluster scored
+p2 <- fit.est$centers %>% #plot means of each cluster, representing how high people each cluster scored
   as_tibble() %>% 
   select(EST01:EST10) %>% 
   mutate(id = row_number()) %>% 
@@ -42,7 +59,7 @@ fit.est$centers %>% #plot means of each cluster, representing how high people ea
   coord_flip()
 
 fit.agr <- kmeans(small, 5)
-fit.agr$centers %>% #plot means of each cluster, representing how high people each cluster scored
+p3 <- fit.agr$centers %>% #plot means of each cluster, representing how high people each cluster scored
   as_tibble() %>% 
   select(AGR01:AGR10) %>% 
   mutate(id = row_number()) %>% 
@@ -52,7 +69,7 @@ fit.agr$centers %>% #plot means of each cluster, representing how high people ea
   coord_flip()
 
 fit.csn <- kmeans(small, 5)
-fit.csn$centers %>% #plot means of each cluster, representing how high people each cluster scored
+p4 <- fit.csn$centers %>% #plot means of each cluster, representing how high people each cluster scored
   as_tibble() %>% 
   select(CSN01:CSN10) %>% 
   mutate(id = row_number()) %>% 
@@ -62,7 +79,7 @@ fit.csn$centers %>% #plot means of each cluster, representing how high people ea
   coord_flip()
 
 fit.opn <- kmeans(small, 5)
-fit.opn$centers %>% #plot means of each cluster, representing how high people each cluster scored
+p5 <- fit.opn$centers %>% #plot means of each cluster, representing how high people each cluster scored
   as_tibble() %>% 
   select(OPN01:OPN10) %>% 
   mutate(id = row_number()) %>% 
