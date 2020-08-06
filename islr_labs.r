6b507021426e514e07df6c060199e30ea9d6b760
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-05 21:47:36 -0700

    kmeans islr

--- islr_labs.r
+++ islr_labs.r
@@ -20,10 +20,23 @@ pr.var <- pr.out$sdev^2
 pr.var
 pve=pr.var/sum(pr.var)
 pve
-par(mfrow = c(1,2))
+par(mfrow = c(2,2))
 plot(pve , xlab=" Principal Component ", ylab="Proportion of Variance Explained ", 
      ylim=c(0,1),type='b')
 plot(cumsum(pve), xlab="Principal Component ", ylab=" Cumulative PVE", 
      ylim=c(0,1), type='b')
 
 # k means ----------------------------------------------------------------------
+set.seed(2)
+x=matrix(rnorm (50*2), ncol=2)
+x[1:25,1]=x[1:25,1]+3
+x[1:25,2]=x[1:25,2]-4
+km.out=kmeans (x,2, nstart =20)
+plot(x, col=(km.out$cluster +1), main="K-Means Clustering Results with K=2", 
+     xlab="", ylab="", pch=20, cex=2)
+set.seed(4)
+km.2=kmeans (x,5, nstart =20) # arg2 signinifies no. of cluster sizes to try, 
+                              # nstart signifies pool of cluster sizes to choose from
+km.2
+km.2$tot.withinss# the total within-cluster sum of squares,
+                 # which we seek to minimize by performing K-means clustering 
