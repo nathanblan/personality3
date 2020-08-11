d533cd671b688015ca92f4ad0bc2d7e62200faeb
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-10 21:18:09 -0700

    update

--- 04-cluster-responses.R
+++ 04-cluster-responses.R
@@ -26,10 +26,21 @@ results <-
 results %>% 
   ggplot(aes(x = clusters, y = percent)) +
   geom_point()
+#>>> The plot reveals that the first 2 clusters explain ~88% of the withiness <<<#
 
 # Step 2: Now fit k-means with the optimal number of clusters
 kmout <- kmeans(results, 3)
+kmout
 # Step 3: Use the code that you sent me to interpret the clusters
+kmout$centers %>% 
+  as_tibble() %>% 
+  select(1:10) %>% 
+  mutate(id = row_number()) %>% 
+  gather(var, val, -id) %>% 
+  ggplot(aes(x = var, y = val, fill = as.factor(id))) +
+  geom_col(position = "dodge") + 
+  coord_flip()
+
 #04 cluster responses for each set if questions
 # For example, get conclusions such as "the first cluster is low on extroversion questions"
 
