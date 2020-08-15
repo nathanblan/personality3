e814615ccea49cc76c55cc4a8f0c4857a4cad46c
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-14 20:07:47 -0700

    grfyehiwj

--- 04.1-pca-responses.R
+++ 04.1-pca-responses.R
@@ -8,6 +8,18 @@ raw <-
   read_rds("data-clean/raw.rds") %>% 
   na.omit()
 
-set.seed(10)
+set.seed(5)
 # PCA --------------------------------------------------------------------------
-pr.out=prcomp(USArrests , scale=TRUE)
+pr.out=prcomp(raw)
+names(pr.out)
+pr.out$center
+pr.out$rotation
+pr.out$sdev
+
+#  variance explained by each principal component
+(pr.var=pr.out$sdev ^2)
+(pve=pr.var/sum(pr.var))
+
+
+#plot PVE explained by each component, as well as the cumulative PVE
+
