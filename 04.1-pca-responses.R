9dd09f1a709ac6e22a08a12b7a1d69312c83c73c
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-17 17:12:27 -0700

    update w comments

--- 04.1-pca-responses.R
+++ 04.1-pca-responses.R
@@ -24,7 +24,7 @@ pr.out$sdev
 (pve=pr.var/sum(pr.var))
 
 
-#cov matrix?
+#manually perform PCA as prcomp does not seem to have any effect
 traw <- as_tibble(t(raw))
 dim(traw)
 dim(raw)
@@ -44,6 +44,7 @@ raw <- raw %>%
   as.matrix()
 dim(raw)
 mm <- raw%*%t(eg) #matrix multiplication result for PCA from raw, compressed for visualization
-plot(mm)
+plot(mm) # plot resulting from PCA
+
 # from this graph we can determine that 2 clusters does seem to reflect a good portion of the data
 # We can use kmeans on "raw" using 2 clusters
