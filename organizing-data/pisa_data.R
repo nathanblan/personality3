43b8cf91ab738e8a74378098a53a171539219938
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-24 18:28:58 -0700

    rename

--- organizing-data/pisa_data.R
+++ organizing-data/pisa_data.R
@@ -8,13 +8,19 @@ library(naniar)
 #read pisa data
 pisa <- 
   read_csv("data-raw/pisa-data.csv")
-View(pisa)
 
 #rename columns
+names(pisa)[1] <- "country"
+names(pisa)[2] <- "c-code"
+names(pisa)[3] <- "series"
+names(pisa)[4] <- "s-code"
+names(pisa)[5] <- "2013"
+names(pisa)[6] <- "2014"
+names(pisa)[7] <- "2015"
 
 #filter for countries with entries
 pisa <- pisa %>% 
-  filter()
+  filter(2015 != "..")
 
 # export as .rds ---------------------------------------------------------------
 r_data %>% write_rds("data-clean/r_data.rds")
