68e528c83f94dbd72b42554d9b27622ee467d339
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-24 18:15:47 -0700

    update

--- organizing-data/pisa_data.R
+++ organizing-data/pisa_data.R
@@ -5,6 +5,16 @@ library(tidyverse)
 library(naniar)
 
 #load PISA data ----------------------------------------------------------------
+#read pisa data
 pisa <- 
   read_csv("data-raw/pisa-data.csv")
 View(pisa)
+
+#rename columns
+
+#filter for countries with entries
+pisa <- pisa %>% 
+  filter()
+
+# export as .rds ---------------------------------------------------------------
+r_data %>% write_rds("data-clean/r_data.rds")
