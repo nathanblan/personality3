7ea0d750ecd283a27ee847db01652326dff74638
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-24 17:55:30 -0700

    hah

--- organizing-data/pisa_data.R
+++ organizing-data/pisa_data.R
@@ -1,1 +1,11 @@
 #PISA Data
+
+#load libraries ----------------------------------------------------------------
+library(tidyverse)
+library(naniar)
+
+#load PISA data ----------------------------------------------------------------
+pisa <- 
+  read_tsv("data-raw/data-final.csv") %>% 
+  mutate(id = row_number()) %>% 
+  select(id, everything())
