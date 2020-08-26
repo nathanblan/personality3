053609df65d73536ccd3a318d635f3d9e29df330
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-25 20:29:01 -0700

    rename

---
+++ 02.1-organizing-data/01-pisa_data.R
@@ -0,0 +1,52 @@
+#PISA Data
+
+#load libraries ----------------------------------------------------------------
+library(tidyverse)
+library(naniar)
+
+#load PISA data ----------------------------------------------------------------
+#read pisa data
+pisa <- 
+  read_csv("data-raw/pisa-data.csv")
+
+#rename columns
+names(pisa)[1] <- "country"
+names(pisa)[2] <- "c_code"
+names(pisa)[3] <- "series"
+names(pisa)[4] <- "s_code"
+names(pisa)[5] <- "2013"
+names(pisa)[6] <- "2014"
+names(pisa)[7] <- "2015"
+
+#filter for countries with entries
+pisa <- pisa %>% 
+  filter(2015 != "..")
+
+#separate tests
+#math
+pisa_math <- pisa %>% 
+  filter(s_code == "LO.PISA.MAT") %>% 
+  arrange(country)
+
+#reading
+pisa_read <- pisa %>% 
+  filter(s_code == "LO.PISA.REA") %>% 
+  arrange(country)
+
+#science
+pisa_sci <- pisa %>% 
+  filter(s_code == "LO.PISA.SCI") %>% 
+  arrange(country)
+
+# export as .rds
+pisa %>% write_rds("data-clean/pisa.rds")
+pisa_math %>% write_rds("data-clean/pisa_math.rds")
+pisa_read %>% write_rds("data-clean/pisa_read.rds")
+pisa_sci %>% write_rds("data-clean/pisa_sci.rds")
+
+#load Country Code data --------------------------------------------------------
+#read country code data
+codes <- 
+  read_csv("data-raw/country_code.csv") %>% 
+  select(-X1)
+
---
+++ 05.1-PCA-clustering/04.2-cluster-responses.R
@@ -0,0 +1,61 @@
+# response clusters ------------------------------------------------------------
+library(tidyverse)  # data manipulation
+library(cluster)    # clustering algorithms
+library(factoextra) # clustering algorithms & visualization
+library(broom)
+
+raw <- 
+  read_rds("data-clean/raw.rds") %>% 
+  na.omit()
+
+small <- raw %>% sample_frac(0.1) %>% select(-id)
+
+set.seed(5)
+# K-means ----------------------------------------------------------------------
+
+# Step 1: Find the optimal number of clusters
+# I wrote some code to get started
+
+custom_kmeans <- function(clusters, data){
+  fit <- kmeans(data, clusters, algorithm = "Lloyd")
+  percent <- fit$betweenss / fit$totss
+  tibble(clusters = clusters, fit = list(fit), percent = percent)
+}
+
+results <- 
+  1:10 %>% 
+  map_df(custom_kmeans, small)
+
+results %>% 
+  ggplot(aes(x = clusters, y = percent)) +
+  geom_line() +
+  geom_point()
+#>>> The plot reveals that the first 2 clusters explain ~88% of the withiness <<<#
+
+# Step 2: Now fit k-means with the optimal number of clusters
+kmout <- kmeans(results, 2)
+small %>%
+  as_tibble() %>%
+  mutate(cluster = kmout$cluster,
+         state = row.names(id)) %>%
+  ggplot(aes(UrbanPop, Murder, color = factor(cluster), label = state)) +
+  geom_text()
+
+# Step 3: Use the code that you sent me to interpret the clusters
+kmout$centers %>% 
+  as_tibble() %>% 
+  select(1:10) %>% 
+  mutate(id = row_number()) %>% 
+  gather(var, val, -id) %>% 
+  ggplot(aes(x = var, y = val, fill = as.factor(id))) +
+  geom_col(position = "dodge") + 
+  coord_flip()
+
+#04 cluster responses for each set if questions
+# For example, get conclusions such as "the first cluster is low on extroversion questions"
+
+# Step 4: Look at how clusters vary across countries etc.
+
+group_by(raw$country)
+raw %>% 
+  plot(color = country)
