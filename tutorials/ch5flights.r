49c94c0f2d422fe80ca16e21aa0600a741841a92
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-26 19:28:49 -0700

    update

--- tutorials/ch5flights.r
+++ tutorials/ch5flights.r
@@ -45,3 +45,19 @@ arrange(flights, year, month, day)
 arrange(flights, desc(dep_delay))
 arrange(flights, is.na(flights))
 ?one_of
+
+flights_sml <- select(flights, 
+                      year:day, 
+                      ends_with("delay"), 
+                      distance, 
+                      air_time
+)
+mutate(flights_sml,
+       gain = dep_delay - arr_delay,
+       speed = distance / air_time * 60
+)
+transmute(flights, #only keeps new variables
+          gain = dep_delay - arr_delay,
+          hours = air_time / 60,
+          gain_per_hour = gain / hours
+)
