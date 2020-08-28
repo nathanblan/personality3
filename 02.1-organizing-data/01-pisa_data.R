acc8e90d801ad5d4a52218406dd81e9672dc3c8f
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-08-28 11:09:54 -0700

    update

--- 02.1-organizing-data/01-pisa_data.R
+++ 02.1-organizing-data/01-pisa_data.R
@@ -7,7 +7,7 @@ library(naniar)
 #load PISA data ----------------------------------------------------------------
 #read pisa data
 pisa <- 
-  read_csv("data-raw/pisa-data.csv")
+  read_csv("01.1-data-raw/pisa-data.csv")
 
 #rename columns
 names(pisa)[1] <- "country"
@@ -39,13 +39,15 @@ pisa_sci <- pisa %>%
   arrange(country)
 
 # export as .rds
-pisa %>% write_rds("data-clean/pisa.rds")
-pisa_math %>% write_rds("data-clean/pisa_math.rds")
-pisa_read %>% write_rds("data-clean/pisa_read.rds")
-pisa_sci %>% write_rds("data-clean/pisa_sci.rds")
+pisa %>% write_rds("01.2-data-clean/pisa.rds")
+pisa_math %>% write_rds("01.2-data-clean/pisa_math.rds")
+pisa_read %>% write_rds("01.2-data-clean/pisa_read.rds")
+pisa_sci %>% write_rds("01.2-data-clean/pisa_sci.rds")
 
 #load Country Code data --------------------------------------------------------
 #read country code data
 codes <- 
-  read_csv("data-raw/country_code.csv") %>% 
+  read_csv("01.1-data-raw/country_code.csv") %>% 
   select(-X1)
+
+#join country data w/ pisa -----------------------------------------------------
