6dec2a6c52495a77ff53e5afc4a97b69c7dc863b
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-09-11 10:21:44 -0700

    updates

--- 04.1-time_responses/time-response-plots.R
+++ 04.1-time_responses/time-response-plots.R
@@ -7,33 +7,33 @@ t1 <- ggplot(world) +
   geom_smooth(aes(x = log(med_EXT_E), y = avg_EXT), method = "lm") +
   geom_text(aes(x = log(med_EXT_E), y = avg_EXT, label = country)) +
   ggsave("plots/extraversion-time~extroversion.png")
-
+t1
 #plot average neuroticism-time vs average neuroticism per country
 t2 <- ggplot(world) +
-  geom_point(aes(x = avg_EST_E, y = avg_EST, color = continent)) +
-  geom_smooth(aes(x = avg_EST_E, y = avg_EST), method = "lm") +
-  geom_text(aes(x = avg_EST_E, y = avg_EST, label = country)) +
+  geom_point(aes(x = log(avg_EST_E), y = avg_EST, color = continent)) +
+  geom_smooth(aes(x = log(avg_EST_E), y = avg_EST), method = "lm") +
+  geom_text(aes(x = log(avg_EST_E), y = avg_EST, label = country)) +
   ggsave("plots/neuroticism-time~neuroticism.png")
 
 #plot average agreeableness-time vs average agreeableness per country
 t3 <- ggplot(world) +
-  geom_point(aes(x = avg_AGR_E, y = avg_AGR, color = continent)) +
-  geom_smooth(aes(x = avg_AGR_E, y = avg_AGR), method = "lm") +
-  geom_text(aes(x = avg_AGR_E, y = avg_AGR, label = country)) +
+  geom_point(aes(x = log(avg_AGR_E), y = avg_AGR, color = continent)) +
+  geom_smooth(aes(x = log(avg_AGR_E), y = avg_AGR), method = "lm") +
+  geom_text(aes(x = log(avg_AGR_E), y = avg_AGR, label = country)) +
   ggsave("plots/agreeableness-time~agreeableness.png")
 
 #plot average conscientiousness-time vs average conscientiousness per country
 t4 <- ggplot(world) +
-  geom_point(aes(x = avg_CSN_E, y = avg_CSN, color = continent)) +
-  geom_smooth(aes(x = avg_CSN_E, y = avg_CSN), method = "lm") +
-  geom_text(aes(x = avg_CSN_E, y = avg_CSN, label = country)) +
+  geom_point(aes(x = log(avg_CSN_E), y = avg_CSN, color = continent)) +
+  geom_smooth(aes(x = log(avg_CSN_E), y = avg_CSN), method = "lm") +
+  geom_text(aes(x = log(avg_CSN_E), y = avg_CSN, label = country)) +
   ggsave("plots/conscientiousness-time~conscientiousness.png")
 
 #plot average openness-time vs average openness per country
 t5 <- ggplot(world) +
-  geom_point(aes(x = avg_OPN_E, y = avg_OPN, color = continent)) +
-  geom_smooth(aes(x = avg_OPN_E, y = avg_OPN), method = "lm") +
-  geom_text(aes(x = avg_OPN_E, y = avg_OPN, label = country)) +
+  geom_point(aes(x = log(avg_OPN_E), y = avg_OPN, color = continent)) +
+  geom_smooth(aes(x = log(avg_OPN_E), y = avg_OPN), method = "lm") +
+  geom_text(aes(x = log(avg_OPN_E), y = avg_OPN, label = country)) +
   ggsave("plots/openness-time~openness.png")
 
 #setup plot layout
--- 05.1-PCA-clustering/05-cluster-responses.R
+++ 05.1-PCA-clustering/05-cluster-responses.R
@@ -1,10 +1,8 @@
 # response clusters ------------------------------------------------------------
-library(tidyverse)  # data manipulation
-library(cluster)    # clustering algorithms
-library(factoextra) # clustering algorithms & visualization
-library(broom)
 library(ggbiplot)
+library(tidyverse)  # data manipulation
 
+#import data -------------------------------------------------------------------
 raw <- 
   read_rds("01.2-data-clean/raw.rds") %>% 
   na.omit() %>% 
@@ -16,7 +14,7 @@ raw <-
 raw.pca <- prcomp(raw %>% 
                     select(-id, -country))
 
-#calculate cumulative std. deviation to identify amount of 
+#calculate cumulative std. deviation to identify useful components
 cum_perc_var_explained <- cumsum(raw.pca$sdev / sum(raw.pca$sdev))
 
 tibble(
@@ -31,14 +29,28 @@ pca_sums <- as_tibble(raw.pca$x[,1:2]) %>%
   mutate(country = raw$country)
 
 #plot PCA
-ggbiplot(raw.pca, alpha = 0.01, varname.size = 10) 
-raw.pca$rotation[,1:2]
+ggbiplot(raw.pca, alpha = 0.01, varname.size = 10) +
+  ggsave("plots/pca-loadings.png")
+raw.pca$rotation[,1:2] #each arrow is a point formed by the values in this chart
+                       #PC1 = x PC2 = y
 
-#summarise PCA
+#summarize PCA
 pca_sums %>% 
   group_by(country) %>% 
   dplyr::summarise(mean_pc1 = mean(PC1),
             mean_pc2 = mean(PC2))
+
+#plot PC1 by country 
+p1 <- ggplot(pca_sums) + 
+  geom_point(aes(x = country, y= PC1)) +
+  coord_flip()
+
+#plot PC2 by country 
+p2 <- ggplot(pca_sums) + 
+  geom_point(aes(x = country, y= PC2)) +
+  coord_flip()
+
+grid.arrange(p1, p2, nrow = 1)
 
 # update world averages --------------------------------------------------------
 # extract world data and join with joint
