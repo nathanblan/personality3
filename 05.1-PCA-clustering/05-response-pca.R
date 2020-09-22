c6311b7d5cd97e2c57c4ea5da6707bb78cd07035
Bryson Nathan Lan <nathanblan.8@gmail.com>
2020-09-22 10:04:31 -0700

    update

--- 05.1-PCA-clustering/05-response-pca.R
+++ 05.1-PCA-clustering/05-response-pca.R
@@ -31,72 +31,132 @@ tibble(
 #extract the first two principle components
 pca_sums <- as_tibble(raw.pca$x[,1:2]) %>% 
   mutate(country = raw$country)
-pca_sums
+head(pca_sums, 10)
+
 #plot PCA
 ggbiplot(raw.pca, alpha = 0.01, varname.size = 10) +
   ggsave("plots/pca-loadings.png")
-raw.pca$rotation[,1:2] #each arrow is a point formed by the values in this chart
+head(raw.pca$rotation[,1:2]) #each arrow is a point formed by the values in this chart
                        #PC1 = x PC2 = y
 
 #summarize PCA
-pca_sums %>% 
+pca_sums <- pca_sums %>% 
   dplyr::group_by(country) %>% 
   dplyr::summarise(mean_pc1 = mean(PC1),
             mean_pc2 = mean(PC2))
-pca_sums
+head(pca_sums, 10)
 
-#pca_sums biplot by country
-pca_sums %>% 
+#pca_sums plot by country
+sums_plot <- pca_sums %>% 
   ggplot(aes(x = mean_pc1, y = mean_pc2, label = country)) +
   geom_point() +
   geom_text(label = pca_sums$country)
+sums_plot
 
 # update world averages --------------------------------------------------------
 # extract world data and join with joint
 world <- world %>% 
   left_join(pca_sums, by = "country") %>%
   as_tibble()
-
-#show loadings
-head(raw.pca$x[,1:2])
+names(world)
 
 #plots -------------------------------------------------------------------------
 #plot average extroversion vs average science per country
 pc1.math <- ggplot(world) +
-  geom_point(aes(x = PC1, y = math, color = continent)) +
-  geom_smooth(aes(x = PC1, y = math), method = "lm") +
-  geom_text(aes(x = PC1, y = math, label = country)) +
+  geom_point(aes(x = mean_pc1, y = math, color = continent)) +
+  geom_smooth(aes(x = mean_pc1, y = math), method = "lm") +
+  geom_text(aes(x = mean_pc1, y = math, label = country)) +
+  stat_regline_equation(
+    aes(x = mean_pc1, y = math, 
+        label =  paste(..eq.label.., sep = "~~~~")),
+    label.x = 3, label.y = 350
+  ) +
+  stat_cor(
+    aes(x = mean_pc1, y = math,
+        label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 1, label.y = 350
+  ) +
   ggsave("plots/PC1~math.png")
 
 pc1.science <- ggplot(world) +
-  geom_point(aes(x = PC1, y = science, color = continent)) +
-  geom_smooth(aes(x = PC1, y = science), method = "lm") +
-  geom_text(aes(x = PC1, y = science, label = country)) +
+  geom_point(aes(x = mean_pc1, y = science, color = continent)) +
+  geom_smooth(aes(x = mean_pc1, y = science), method = "lm") +
+  geom_text(aes(x = mean_pc1, y = science, label = country)) +
+  stat_regline_equation(
+    aes(x = mean_pc1, y = science, 
+        label =  paste(..eq.label.., sep = "~~~~")),
+    label.x = 3, label.y = 350
+  ) +
+  stat_cor(
+    aes(x = mean_pc1, y = science,
+        label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 1, label.y = 350
+  ) +
   ggsave("plots/PC1~science.png")
 
 pc1.reading <- ggplot(world) +
-  geom_point(aes(x = PC1, y = reading, color = continent)) +
-  geom_smooth(aes(x = PC1, y = reading), method = "lm") +
-  geom_text(aes(x = PC1, y = reading, label = country)) +
+  geom_point(aes(x = mean_pc1, y = reading, color = continent)) +
+  geom_smooth(aes(x = mean_pc1, y = reading), method = "lm") +
+  geom_text(aes(x = mean_pc1, y = reading, label = country)) +
+  stat_regline_equation(
+    aes(x = mean_pc1, y = reading, 
+        label =  paste(..eq.label.., sep = "~~~~")),
+    label.x = 3, label.y = 350
+  ) +
+  stat_cor(
+    aes(x = mean_pc1, y = reading,
+        label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 1, label.y = 350
+  ) +
   ggsave("plots/PC1~reading.png")
 
 #plot PC2 vs average math per country
 pc2.math <- ggplot(world) +
-  geom_point(aes(x = PC2, y = math, color = continent)) +
-  geom_smooth(aes(x = PC2, y = math), method = "lm") +
-  geom_text(aes(x = PC2, y = math, label = country)) +
+  geom_point(aes(x = mean_pc2, y = math, color = continent)) +
+  geom_smooth(aes(x = mean_pc2, y = math), method = "lm") +
+  geom_text(aes(x = mean_pc2, y = math, label = country)) +
+  stat_regline_equation(
+    aes(x = mean_pc2, y = math, 
+        label =  paste(..eq.label.., sep = "~~~~")),
+    label.x = 3, label.y = 350
+  ) +
+  stat_cor(
+    aes(x = mean_pc2, y = math,
+        label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 1, label.y = 350
+  ) +
   ggsave("plots/PC2~math.png")
 
 pc2.science <- ggplot(world) +
-  geom_point(aes(x = PC2, y = science, color = continent)) +
-  geom_smooth(aes(x = PC2, y = science), method = "lm") +
-  geom_text(aes(x = PC2, y = science, label = country)) +
+  geom_point(aes(x = mean_pc2, y = science, color = continent)) +
+  geom_smooth(aes(x = mean_pc2, y = science), method = "lm") +
+  geom_text(aes(x = mean_pc2, y = science, label = country)) +
+  stat_regline_equation(
+    aes(x = mean_pc2, y = science, 
+        label =  paste(..eq.label.., sep = "~~~~")),
+    label.x = 3, label.y = 350
+  ) +
+  stat_cor(
+    aes(x = mean_pc2, y = science,
+        label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 1, label.y = 350
+  ) +
   ggsave("plots/PC2~science.png")
 
 pc2.reading <- ggplot(world) +
-  geom_point(aes(x = PC2, y = reading, color = continent)) +
-  geom_smooth(aes(x = PC2, y = reading), method = "lm") +
-  geom_text(aes(x = PC2, y = reading, label = country)) +
+  geom_point(aes(x = mean_pc2, y = reading, color = continent)) +
+  geom_smooth(aes(x = mean_pc2, y = reading), method = "lm") +
+  geom_text(aes(x = mean_pc2, y = reading, label = country)) +
+  stat_regline_equation(
+    aes(x = mean_pc2, y = reading, 
+        label =  paste(..eq.label.., sep = "~~~~")),
+    label.x = 3, label.y = 350
+  ) +
+  stat_cor(
+    aes(x = mean_pc2, y = reading,
+        label = paste(..rr.label.., ..p.label.., sep = "~`,`~")),
+    label.x = 1, label.y = 350
+  ) +
   ggsave("plots/PC2~reading.png")
 
 #setup plot layout pc1
